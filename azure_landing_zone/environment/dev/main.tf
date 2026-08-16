3c99a460d1e8dbdd808565cab0bd3567ff8a4dc0
Amit Pradhan <amitkumarpradhan1818@gmail.com>
2026-08-16 08:46:53 +0530

    Code added

---
+++ azure_landing_zone/environment/dev/main.tf
@@ -0,0 +1,13 @@
+module "Resource_group" {
+  source = "../../module/azurerm_resource_group"
+
+  rgs = var.my_rgs
+}
+
+module "Storage_Account" {
+  depends_on = [module.Resource_group]
+
+  source = "../../module/azurerm_storage_account"
+
+  sas = var.my_sas
+}
